8d1f01b3365b7774b7a3290d372b68c8bfde2883
Caio Merotti <caio_merotti@hotmail.com>
2024-05-07 22:22:00 -0300

    fix parameter

--- src/main/java/com.github/client/ClientHttpConfiguration.java
+++ src/main/java/com.github/client/ClientHttpConfiguration.java
@@ -1,6 +1,6 @@
 package com.github.client;
 
-import com.google.gson.JsonObject;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.net.URI;
@@ -20,11 +20,11 @@ public class ClientHttpConfiguration {
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
-    public HttpResponse<String> requestPOST(String uri, JsonObject payload) throws IOException, InterruptedException {
+    public HttpResponse<String> requestPOST(String uri, Object payload) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .header("Content-Type", "application/json")
-                .method("POST", HttpRequest.BodyPublishers.ofString(payload.toString()))
+                .method("POST", HttpRequest.BodyPublishers.ofString(new Gson().toJson(payload)))
                 .build();
 
         HttpClient client = HttpClient.newHttpClient();
